69eadeb4ab000c48209aefb293de11165e0d8030
Mike Grant <mike-grant@users.noreply.github.com>
2023-07-16 18:54:46 +0000

    Fix docusaurus

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://mike-grant.github.io/intelliextract/',
+  url: 'https://mike-grant.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/intelliextract/',
